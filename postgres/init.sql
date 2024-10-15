a1a85816e9a5c83103c2ac0b9fee618af64a4510
agarmirus <tsarevantuan@yandex.ru>
2024-10-15 10:59:09 +0000

    Added data generation and cleaning procedures

--- postgres/init.sql
+++ postgres/init.sql
@@ -6,3 +6,25 @@ CREATE TABLE person
     address TEXT NOT NULL CHECK (TRIM(address) != ''),
     work TEXT NOT NULL CHECK (TRIM(work) != '')
 );
+
+CREATE PROCEDURE generate_test_data()
+LANGUAGE SQL
+AS
+$$
+    ALTER SEQUENCE person_id_seq RESTART WITH 1;
+    INSERT INTO person
+    VALUES
+        ("Andrew", 19, "Moscow", "IT"),
+        ("Sergey", 23, "Rostov", "Engineering"),
+        ("Ivan", 35, "Saint Petersburg", "Science"),
+        ("Anton", 22, "Moscow", "IT"),
+        ("Elena", 27, "Kazan", "IT");
+$$;
+
+CREATE PROCEDURE clear_tables()
+LANGUAGE SQL
+AS
+$$
+    DELETE TABLE person;
+    ALTER SEQUENCE person_id_seq RESTART WITH 1;
+$$;
